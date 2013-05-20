21fb1fdbed03bfea12814a8bcc8199e7fdc08621
Wojciech Sznapka <wojciech.sznapka@xsolve.pl>
2013-05-20 10:13:27 +0200

    C# implementation by Jarosław Kroczek

---
+++ 01-roman-numerals/RomanConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanConverter
+{
+    public class RomanConverter
+    {
+        protected Dictionary<int, string> conversions;
+
+        public RomanConverter()
+        {
+            conversions = new Dictionary<int, string>()
+            {
+                { 1000, "M" },
+                { 900, "CM" },
+                { 500, "D" },
+                { 400, "CD" },
+                { 100, "C" },
+                { 90, "XC" },
+                { 50, "L" },
+                { 40, "XL" },
+                { 10, "X" },
+                { 9, "IX" },
+                { 5, "V" },
+                { 4, "IV" },
+                { 1, "I" }
+            };
+
+        }
+        
+        public string Convert(int arabic)
+        {
+            string roman = "";
+
+            foreach (var conv in conversions)
+            {
+                while (arabic >= conv.Key)
+                {
+                    roman += conv.Value;
+                    arabic -= conv.Key;
+                }
+                if (arabic == 0)
+                    break;
+            }
+            return roman;
+        }
+    }
+}
---
+++ 01-roman-numerals/RomanConverterUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanConverterTests
+{
+    [TestClass]
+    public class RomanConverterUnitTest
+    {
+        [TestMethod]
+        public void SimpleNumbersShouldEqual()
+        {
+            var conv = new RomanConverter.RomanConverter();
+            
+            Assert.AreEqual("I", conv.Convert(1));
+            Assert.AreEqual("V", conv.Convert(5));
+            Assert.AreEqual("X", conv.Convert(10));
+        }
+
+        [TestMethod]
+        public void ComplexNumbersShouldEqual()
+        {
+            var conv = new RomanConverter.RomanConverter();
+            
+            Assert.AreEqual("II", conv.Convert(2));
+            Assert.AreEqual("LXXXVIII", conv.Convert(88));
+            Assert.AreEqual("DI", conv.Convert(501));
+            Assert.AreEqual("DCCCXC", conv.Convert(890));
+            Assert.AreEqual("MDCCC", conv.Convert(1800));
+            Assert.AreEqual("MMDCLXIV", conv.Convert(2664));
+        }
+    }
+}
